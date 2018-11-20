c4fadef54d75e64cf71cc8a16a71d907a0bdc6c9
abe <ta-b0@users.noreply.github.com>
2018-11-20 18:46:57 +0900

    Update Make_WinPEx86.bat

--- Make_WinPEx86.bat
+++ Make_WinPEx86.bat
@@ -1,6 +1,16 @@
 copype x86 C:\WinPE_x86
 Dism /Mount-Image /ImageFile:"C:\WinPE_x86\media\sources\boot.wim" /index:1 /MountDir:"C:\WinPE_x86\mount"
 Dism /Set-ScratchSpace:256 /Image:"C:\WinPE_x86\mount"
+rem Language=Japanese
+dism /image:"C:\WinPE_amd64\mount" /set-allintl:ja-jp
+rem Keyboard=US
+dism /image:"C:\WinPE_amd64\mount" /Set-LayeredDriver:1
+dism /image:"C:\WinPE_amd64\mount" /Set-InputLocale:0409:00000409
+rem Keyboard=JIS
+rem dism /image:"C:\WinPE_amd64\mount" /Set-LayeredDriver:6
+rem dism /image:"C:\WinPE_amd64\mount" /Set-InputLocale:ja-jp
+rem dism/image:"C:\WinPE_amd64\mount" /Set-InputLocale:0411:{03B5835F-F03C-411B-9CE2-AA23E1171E36}{A76C93D9-5523-4E90-AAFA-4DB112F9AC76}
+rem Set Options
 Dism /image:C:\WinPE_x86\mount /add-package /packagepath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-MDAC.cab"
 Dism /image:C:\WinPE_x86\mount /add-package /packagepath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\ja-jp\WinPE-MDAC_ja-jp.cab"
 Dism /image:C:\WinPE_x86\mount /add-package /packagepath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs\WinPE-FMAPI.cab"
